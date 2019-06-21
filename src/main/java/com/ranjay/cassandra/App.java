a61dbd43b4a8ed8fdcc2c0c94d77eb8a969c057e
SalmonRanjay <ranjy23@gmail.com>
2019-06-20 22:03:44 -0400

    Update App.java

--- src/main/java/com/ranjay/cassandra/App.java
+++ src/main/java/com/ranjay/cassandra/App.java
@@ -7,6 +7,7 @@ import com.ranjay.cassandra.models.SessionEvents;
 import com.ranjay.cassandra.models.SongSession;
 import com.ranjay.cassandra.models.UserSession;
 import com.ranjay.cassandra.services.CassandraService;
+import com.ranjay.cassandra.services.CassandraServiceSingleton;
 import com.ranjay.cassandra.services.FileService;
 
 /**
@@ -16,31 +17,21 @@ import com.ranjay.cassandra.services.FileService;
 public class App {
 
         public static void main(String[] args) {
-               
-                System.out.println();
 
-               
-                // FileService.readCSVFile(new File("./data"))
-                // .map((item) -> new SessionEvents(item))
-                // .forEach(item -> CassandraService
-                //         .mapSessionEventPojoToCQLQuery.accept(item));
-                
+                CassandraServiceSingleton cassandraService = CassandraServiceSingleton.getInstance();
 
-                FileService.readCSVFile(new File("./data"))
-                                .map( (item) -> new UserSession(item))
-                                .forEach(item -> CassandraService
-                                        .mapUserEventPojoToCQLQuery.accept(item));
-                
+                // FileService.readCSVFile(new File("./data")).map((eventData) -> new SessionEvents(eventData))
+                //                 .forEach(item -> cassandraService.mapSessionEventPojoToCQLQuery.accept(item));
 
-                // FileService.readCSVFile(new File("./data"))
-                //                 .map((item) ->  new SongSession(item))
-                //                 .forEach(item -> CassandraService
-                //                         .mapSongEventPojoToCQLQuery.accept(item));
-                
+                // FileService.readCSVFile(new File("./data")).map((eventData) -> new UserSession(eventData))
+                //                 .forEach(item -> cassandraService.mapUserEventPojoToCQLQuery.accept(item));
 
-                // CassandraService.dropTables();
-        
-                CassandraService.closeResources();
+                // FileService.readCSVFile(new File("./data")).map((eventData) -> new SongSession(eventData))
+                //                 .forEach(item -> cassandraService.mapSongEventPojoToCQLQuery.accept(item));
+
+                cassandraService.dropTables();
+
+                cassandraService.closeResources();
 
                 System.out.println("KEYSPACE: events");
                 System.out.println("TABLES: sessionevents, usersessions, songsession");
